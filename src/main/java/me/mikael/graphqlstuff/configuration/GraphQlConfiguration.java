35936ebb49b7980b6545755fa42ef2a2eb431561
mikael <mail.micke@gmail.com>
2026-01-31 15:08:59 +0000

    tinkering

--- src/main/java/me/mikael/graphqlstuff/configuration/GraphQlConfiguration.java
+++ src/main/java/me/mikael/graphqlstuff/configuration/GraphQlConfiguration.java
@@ -4,9 +4,29 @@ import org.springframework.boot.autoconfigure.graphql.GraphQlSourceBuilderCustom
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.graphql.data.federation.FederationSchemaFactory;
+import org.springframework.graphql.execution.RuntimeWiringConfigurer;
+import me.mikael.graphqlstuff.entity.Customer;
+
+import java.util.Optional;
 
 @Configuration
 public class GraphQlConfiguration {
+
+    // Exmple adding a custom field and wiring the logic manually
+    @Bean
+    public RuntimeWiringConfigurer runtimeWiringConfigurer() {
+        return wiringBuilder -> wiringBuilder
+                .type("Customer", builder -> builder
+                        .dataFetcher("greeting", env -> {
+                            Customer customer = env.getSource();
+                            return "Hello, "
+                                    + Optional.ofNullable(customer)
+                                    .map(Customer::getName)
+                                    .orElse("Anonymous")
+                                    + "!";
+                        })
+                );
+    }
 
     @Bean
     public GraphQlSourceBuilderCustomizer customizer(FederationSchemaFactory federationSchemaFactory) {
